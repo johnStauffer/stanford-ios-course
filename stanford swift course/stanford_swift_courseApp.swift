dc47f0f6b27903d85ea3e07fa7bcdef66d1f09ad
John Stauffer <john.stauffer.d@gmail.com>
2021-01-01 14:33:59 -0600

    Initial Commit

---
+++ stanford swift course/stanford_swift_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanford_swift_courseApp.swift
+//  stanford swift course
+//
+//  Created by John Stauffer on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct stanford_swift_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stanford swift courseTests/stanford_swift_courseTests.swift
@@ -0,0 +1,33 @@
+//
+//  stanford_swift_courseTests.swift
+//  stanford swift courseTests
+//
+//  Created by John Stauffer on 1/1/21.
+//
+
+import XCTest
+@testable import stanford_swift_course
+
+class stanford_swift_courseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
